54b99bddcff0d42b021d1a9542412d732623f118
MattAgn <matthieua@bam.tech>
2023-11-03 20:10:43 +0100

    feat: display movie titles

--- src/app/navigation/Home.screen.tsx
+++ src/app/navigation/Home.screen.tsx
@@ -1,25 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const API_KEY = '1d868d05865a228a5fb2fc24c37d7b36';
+const API_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=fr-FR&region=FR`;
 
 export type HomeRouteParams = undefined;
 
 export const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const json = await response.json();
+        setMovies(json.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+    </View>
+  );
+
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>Movies App</Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ alignSelf: 'center', fontSize: 20 }}>Recent movies</Text>
+
+      <FlatList
+        data={movies}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
+  },
+  item: {
+    backgroundColor: 'lightblue',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 16,
+    flexShrink: 1,
   },
 });
