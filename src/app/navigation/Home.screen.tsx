652e1b003484ea931ea261fa9b4cd21528dfef11
MattAgn <matthieua@bam.tech>
2023-11-03 20:30:57 +0100

    feat: display images and movie number

--- src/app/navigation/Home.screen.tsx
+++ src/app/navigation/Home.screen.tsx
@@ -1,6 +1,13 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 const API_KEY = '1d868d05865a228a5fb2fc24c37d7b36';
 const API_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=fr-FR&region=FR`;
@@ -9,6 +16,7 @@ export type HomeRouteParams = undefined;
 
 export const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [nbMovies, setNbMovies] = useState<number>();
 
   useEffect(() => {
     axios({ method: 'get', url: API_URL }).then((json) =>
@@ -16,8 +24,17 @@ export const Home = () => {
     );
   }, []);
 
+  useEffect(() => {
+    const nbMv = movies.length;
+    setNbMovies(nbMv);
+  }, [movies]);
+
   const renderItem = ({ item }) => (
     <View style={styles.item}>
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{item.title}</Text>
     </View>
   );
@@ -25,6 +42,7 @@ export const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{ alignSelf: 'center', fontSize: 20 }}>Recent movies</Text>
+      <Text>Number of movies displayed: {nbMovies}</Text>
 
       <FlatList
         data={movies}
@@ -50,5 +68,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 16,
     flexShrink: 1,
+  },
+  image: {
+    width: 50,
+    height: 75,
+    marginRight: 10,
   },
 });
